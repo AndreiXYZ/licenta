0eff775b136329ab0deb7ad437c82b91ff0e6d85
Apostol Andrei <apo.andrei23@yahoo.com>
2018-05-14 02:42:28 +0300

    built svm classifier for sign/background classifier. 98.9 test acc., should test on real images and see results

--- image_segmentation/build_background_set.py
+++ image_segmentation/build_background_set.py
@@ -1,17 +1,14 @@
 from helpers import *
 import random
 import sys
-rootdir = '/home/apo/Licenta/image_segmentation/FullIJCNN2013/for_test_set'
+rootdir = '/home/apo/Licenta/image_segmentation/raw_images/test'
 background_set_path = '/home/apo/Licenta/image_segmentation/background_set_multi/test'
 
-dimensions = [24, 48, 72, 96, 120]
+dimensions = [32, 48, 64, 80, 96, 112, 128]
 
 #build 3000 image background set
 for elem in os.listdir(rootdir):
 	print(elem)
-	if len(os.listdir(background_set_path)) > 2500:
-		print('Done')
-		break
 	if elem[-3:] == 'ppm':
 			path = os.path.join(rootdir, elem)
 			image = plt.imread(path)
@@ -19,7 +16,7 @@ for elem in os.listdir(rootdir):
 			dim = random.choice(dimensions)
 			windows = list(sliding_window(image, dim, (dim,dim)))
 			#sample from the total number of windows to avoid balancing issues
-			sample_windows = random.sample(windows, 50)
+			sample_windows = random.sample(windows, 65)
 			for y,x,img in sample_windows:
 				if img.shape[0] == img.shape[1]:
 					img_name = os.path.join(background_set_path, elem[:-4])
